0bc34d4fb5a7b16f49a70ee63b27fbe4fe7a53b6
rx1310 <rx1310@inbox.ru>
2021-05-16 11:00:07 +0300

    Небольшие правки

--- library/src/main/java/com/github/a2iga/lib/A2IGA.java
+++ library/src/main/java/com/github/a2iga/lib/A2IGA.java
@@ -1,6 +1,7 @@
+// ! rx1310 <rx1310@inbox.ru> | Copyright (c) rx1310, 2020 | MIT License
+
 package com.github.a2iga.lib;
 
-public class A2IGA
-{
+public class A2IGA {
 	public static String testString = "Hello, world!";
 }
