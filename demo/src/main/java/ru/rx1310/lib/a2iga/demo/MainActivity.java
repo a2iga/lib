f9a00275e8e813f36b6ba9093772d1e2ddbe20aa
rx1310 <rx1310@inbox.ru>
2021-05-16 10:55:18 +0300

    Добавил demo-проект

---
+++ demo/src/main/java/ru/rx1310/lib/a2iga/demo/MainActivity.java
@@ -0,0 +1,17 @@
+package ru.rx1310.lib.a2iga.demo;
+
+import android.app.*;
+import android.os.*;
+import com.github.a2iga.lib.A2IGA;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+		
+		
+    }
+}
